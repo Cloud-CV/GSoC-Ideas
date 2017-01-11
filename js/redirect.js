c9a90b4533b9fb3e454592bb545ec1d27f5bdf52
Viraj Prabhu <prabhuviraj@gmail.com>
2017-01-11 11:27:19 -0500

    Removed redirect

--- js/redirect.js
+++ js/redirect.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
-  // This is a hack that our old github.io URL redirects to our new 101.o.o
-  if(window.location.href.indexOf('github.io') > -1){
-    window.location.replace('http://101.opensuse.org/');
-  }
-});
+// $(document).ready(function () {
+//   // This is a hack that our old github.io URL redirects to our new 101.o.o
+//   if(window.location.href.indexOf('github.io') > -1){
+//     window.location.replace('http://101.opensuse.org/');
+//   }
+// });
